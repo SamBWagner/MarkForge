6c531c7a742f105105adc238891443b7efff189e
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-10-29 08:32:29 +1000

    ✨Initial commit

---
+++ MarkForge/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Goal: Have a CLI tool which takes in a list of files as input and generates html to an associated location.
+ * Steps to completion:
+ * 1. Read markdown
+ *   - Handle all base specifications of Markdown. e.g. H1, H2, quote, etc
+ * 2. Create HTML
+ *   - Based on where the HTML is meant to go and the markdown provided, generate some HTML
+ * 3. Make it Build Pipeline friendly
+ */
+ 
+// Read files
+
+// log the markdown elements
+
+Console.WriteLine("Welcome to the app!");
